13a371022227fddfc5d467f5b291d2647ef5d3e4
eCOM-karthik.v3 <ecom-karthik.v3@LT-D-kavi-MAC.local>
2019-03-13 16:18:46 +0530

    Basic child component which can be used as npm module is done

---
+++ child_component/Components/Content.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <div>
+                Body content
+            </div>
+        )
+    }
+}
+
+export default Content;
